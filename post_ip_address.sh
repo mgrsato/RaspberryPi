c2f7a3bfd6e09d9ddf7c16825c1a2bb9e3c8c167
mgrsato <mgrsato@gmail.com>
2019-04-16 00:38:57 +0900

    Update post_ip_address.sh

--- post_ip_address.sh
+++ post_ip_address.sh
@@ -5,10 +5,8 @@ eth0=`ip a show eth0 | grep "inet " | cut -d " " -f 6 | cut -d "/" -f 1`
 
 URL="https://maker.ifttt.com/trigger"
 EVENTNAME="raspberry_pi"
-YOUR_KEY="i_BoH2-Jv_YogejwjkPlK"
+YOUR_KEY="＊＊＊＊＊＊"
 
 curl -X POST "${URL}/${EVENTNAME}/with/key/${YOUR_KEY}?value1=${wlan0}&value2=${eth0}&value3=${EVENTNAME}"
 
-#echo $wlan0;
-#echo $eth0;
 exit 0
